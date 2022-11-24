c71f484cf4bfeda40a64884b0d5ad662559eb098
giannis <bkgiannis8@gmail.com>
2022-11-24 17:01:43 +0200

    Make the code work for index and contact html files and the links in them

--- index.js
+++ index.js
@@ -4,9 +4,20 @@ const fs = require('fs');
 
 http.createServer(function (req, res) {
     //if the link clicked is index or is the first time in the page load index.html
-    fs.readFile('index.html', (err, data) => {
-        res.writeHead(200, {'Content-Type': 'text/html'});
-        res.write(data);
-        return res.end();
-    }).listen(8080);
-}).listen(8080)
+    if (req.url==='/') {
+        fs.readFile('index.html', (err, data) => {
+            if (err) throw err;
+            res.writeHead(200, {'Content-Type':'text/html'});
+            res.write(data);
+            return res.end();
+        })
+    }
+    else if (req.url==='/contact-me') {
+        fs.readFile('contact-me.html', (err, data) => {
+            if (err) throw err;
+            res.writeHead(200, {'Content-Type':'text/html'});
+            res.write(data);
+            return res.end();
+        })
+    }
+}).listen(8080);
